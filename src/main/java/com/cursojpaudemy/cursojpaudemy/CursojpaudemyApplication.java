424f92ff3e9d2d14eabe4e315008f1c08258cfbe
Magnum2193 <rodriigo.magnum@gmail.com>
2022-09-14 21:38:00 -0300

    Project created

---
+++ src/main/java/com/cursojpaudemy/cursojpaudemy/CursojpaudemyApplication.java
@@ -0,0 +1,13 @@
+package com.cursojpaudemy.cursojpaudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursojpaudemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursojpaudemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursojpaudemy/cursojpaudemy/CursojpaudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojpaudemy.cursojpaudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursojpaudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
